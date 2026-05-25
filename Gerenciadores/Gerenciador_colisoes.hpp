4caaa66dea77d29cf9fa8e3d03c36d8b710604e2
Vitor Cruz <vitorcolatusso@gmail.com>
2026-05-25 01:29:52 -0300

    Trabalhando na classe colisoes

--- Gerenciadores/Gerenciador_colisoes.hpp
+++ Gerenciadores/Gerenciador_colisoes.hpp
@@ -1,13 +1,32 @@
 #pragma once
+#include <vector>
+#include <list>
+#include <set>
+using namespace std;
+
+namespace Entidades{
+    class Projetil;
+    namespace Obstaculos{
+        class Obstaculo;
+    }
+    namespace Personagens{
+        class Inimigo;
+    }
+    class Jogador;
+}
 
 namespace Gerenciadores{
 
 //CLASSE GERENCIADOR DE COLISOES, IMPLEMENTAR
 class Gerenciador_Colisoes{
 private:
-
+    vector<Entidades::Personagens::Inimigo*>LIs;
+    list<Entidades::Obstaculos::Obstaculo*>LOs;
+    set<Entidades::Projetil*>LPs;
+    Entidades::Jogador* pJog1;
+    Entidades::Jogador* pJog2;
 public:
-
+    
 };
 
 }
