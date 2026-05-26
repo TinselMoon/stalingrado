0b354f6a52ab11ac71a3f41bb6e226e202202e99
Vitor Cruz <vitorcolatusso@gmail.com>
2026-05-25 23:34:17 -0300

    namespaces e arquivos corrigidos

--- stalingrado/Jogo.hpp
+++ stalingrado/Jogo.hpp
@@ -6,21 +6,21 @@ namespace Stalingrado{
     namespace Entidades{
 
         namespace Personagens{
-            //Forward declaration para usar ponteiro
-            class Jogador;
-            class Inim_facil;
-
-            class Jogo{
-            private:
-                Gerenciadores::Gerenciador_Grafico GG;
-                Menu mJogo;
-                Jogador* pJog1;
-                Inim_facil* pInim1; //Implementação do Gustavo
-            public:
-                Jogo();
-                ~Jogo();
-                void executar();
-            };
+        //Forward declaration para usar ponteiro
+        class Jogador;
+        class Inim_facil;
         }
     }
+
+    class Jogo{
+    private:
+        Gerenciadores::Gerenciador_Grafico GG;
+        Menu mJogo;
+        Entidades::Personagens::Jogador* pJog1;
+        Entidades::Personagens::Inim_facil* pInim1; //Implementação do Gustavo
+    public:
+        Jogo();
+        ~Jogo();
+        void executar();
+    };
 }
