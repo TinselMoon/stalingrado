7e0672f41bfa9e46b2572299c6ee1a79c473c742
Vitor Cruz <vitorcolatusso@gmail.com>
2026-05-25 22:50:23 -0300

    Namespaces e hierarquias de pastas corrigidas

--- stalingrado/Gerenciadores/Gerenciador_grafico.hpp
+++ stalingrado/Gerenciadores/Gerenciador_grafico.hpp
@@ -2,23 +2,22 @@
 
 #include <SFML/Graphics.hpp>
 
-//Pré declaração do namespace aqui para usar os ponteiros e etc
 namespace Stalingrado {
+//Pré declaração do namespace aqui para usar os ponteiros e etc
     class Ente;
-}
 
-namespace Gerenciadores {
-class Gerenciador_Grafico {
-private:
-    sf::RenderWindow janela;
+    namespace Gerenciadores {
+    class Gerenciador_Grafico {
+    private:
+        sf::RenderWindow janela;
 
-public:
-    Gerenciador_Grafico();
-    ~Gerenciador_Grafico();
-    void desenharEnte (Stalingrado::Ente *pE);
-    void desenhar(sf::RectangleShape* forma);
-    sf::RenderWindow* getJanela();   
-    void executar(); //interpretacao Gustavo
-};
-
+    public:
+        Gerenciador_Grafico();
+        ~Gerenciador_Grafico();
+        void desenharEnte (Stalingrado::Ente *pE);
+        void desenhar(sf::RectangleShape* forma);
+        sf::RenderWindow* getJanela();   
+        void executar(); //interpretacao Gustavo
+    };
+    }
 }
