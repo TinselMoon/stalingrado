5fbbbe68d70ea9688a34ed983efdcd0e8cb3d2c6
gswilker <gustavowilker@alunos.utfpr.edu.br>
2026-06-18 14:54:42 -0300

    fazendo menu e botoes

---
+++ stalingrado/Menus/Botao.cpp
@@ -0,0 +1,5 @@
+//
+// Created by x on 6/18/26.
+//
+
+#include "Botao.hpp"
---
+++ stalingrado/Menus/Botao.hpp
@@ -0,0 +1,8 @@
+//
+// Created by x on 6/18/26.
+//
+
+#ifndef STALINGRADO_BOTAO_HPP
+#define STALINGRADO_BOTAO_HPP
+
+#endif //STALINGRADO_BOTAO_HPP
