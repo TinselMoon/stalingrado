cbf39599923d27b370593a3d54536e2758da1195
Vitor Cruz <vitorcolatusso@gmail.com>
2026-06-20 15:38:36 -0300

    corrigido pontos fase 2

--- stalingrado/States/Fases/Fase_seg.cpp
+++ stalingrado/States/Fases/Fase_seg.cpp
@@ -192,7 +192,7 @@ namespace Stalingrado {
             vida1 << "Vida \tJogador \t1:\t " << pJog1->getVida() << "\t PONTOS:\t" << pJog1->getPontos();
             pGG->desenharTextoCamera(vida1.str(), 20, 50.f, 10.f);
             if(pJog2 != nullptr){
-                vida1 << "Vida \tJogador \t1:\t " << pJog1->getVida() << "\t PONTOS:\t" << pJog1->getPontos();
+                vida2 << "Vida \tJogador \t2:\t " << pJog2->getVida() << "\t PONTOS:\t" << pJog2->getPontos();
                 pGG->desenharTextoCamera(vida2.str(), 20, 50.f, 50.f);
             }
         }
